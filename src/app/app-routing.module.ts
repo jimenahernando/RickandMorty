2ed3657a96a28b549f93996b9f3952018e5e2d66
jimena <jimenahernando@hotmail.com>
2021-08-30 21:21:52 +0200

    Creo componentes y rutas.Importo lib httpClient

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { InfoCharacterComponent } from './components/info-character/info-character.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/home" },
+  { path: "home", component: HomeComponent },
+  { path: "character/:idCharacter", component: InfoCharacterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { InfoCharacterComponent } from './components/info-character/info-character.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    HomeComponent,
+    InfoCharacterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
