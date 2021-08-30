0f625f71d75e6c672b3206677ad954cce7028f9b
jimena <jimenahernando@hotmail.com>
2021-08-30 23:13:45 +0200

    Componentes hijos: episodes y locations con sus rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { InfoCharacterComponent } from './components/info-character/info-character.component';
+import { EpisodesComponent } from './components/info-character/episodes/episodes.component';
+import { LocationsComponent } from './components/info-character/locations/locations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
     HomeComponent,
-    InfoCharacterComponent
+    InfoCharacterComponent,
+    EpisodesComponent,
+    LocationsComponent
   ],
   imports: [
     BrowserModule,
